d4dbe645b36cfeddd76354c0b09cd3285cece575
Gosick96 <arqcompu4@gmail.com>
2019-04-11 17:26:46 -0500

    first merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from '../shared/app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from 'src/shared/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
