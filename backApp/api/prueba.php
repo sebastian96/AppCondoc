5490204d4b29ff4f9afb14503e6f9ac40e7da97c
Sebastian Miranda <arqcompu4@gmail.com>
2019-05-01 22:32:29 -0500

    NEW: back modificado con slim y nuevo servicio de authentication

---
+++ backApp/api/prueba.php
@@ -0,0 +1,77 @@
+<?php
+    use \Psr\Http\Message\ServerRequestInterface as Request;
+    use \Psr\Http\Message\ResponseInterface as Response;
+    use \Firebase\JWT\JWT;
+    require '../vendor/autoload.php';
+    include '../config/conexion.php';
+
+    $app = new \Slim\App;
+
+    // FUNCION DE PRUEBA PARA INICIO DE SLIM ↓↓↓
+    $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+        $name = $args['name'];
+        $response->getBody()->write("Hello, $name");
+
+        return $response;
+    });
+
+    $app->post('/login', function(Request $request, Response $response, array $args){
+        $data = $request->getParsedBody();
+
+$privateKey = <<<EOD
+-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
+vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
+5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
+AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
+bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
+Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
+cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
+5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
+ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
+k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
+qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
+eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
+B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
+-----END RSA PRIVATE KEY-----
+EOD;
+$publicKey = <<<EOD
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8kGa1pSjbSYZVebtTRBLxBz5H
+4i2p/llLCrEeQhta5kaQu/RnvuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t
+0tyazyZ8JXw+KgXTxldMPEL95+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4
+ehde/zUxo6UvS7UrBQIDAQAB
+-----END PUBLIC KEY-----
+EOD;
+
+        $user = $data['user'];
+        $password = $data['password'];
+
+        $respuesta = [];
+
+        $obj_db = new DataBase();
+
+        $consulta = "SELECT * FROM usuarios WHERE usuario = '$user' AND password = '$password'";
+        $obj_db->query($consulta);
+        $result = $obj_db->register();
+
+        if($obj_db->rowCount() > 0) {
+            $token = Array(
+                "nombre" => $result->nombre,
+                "apellido" => $result->apellido,
+                "user" => $result->usuario,
+                "estado" => "success"
+            );
+        } else {
+            $token = Array(
+                "estado" => "error"
+            );
+        }
+
+        $jwt = JWT::encode($token, $privateKey, 'RS256');
+
+        echo json_encode($jwt);
+    });
+
+    $app->run();
+?>
