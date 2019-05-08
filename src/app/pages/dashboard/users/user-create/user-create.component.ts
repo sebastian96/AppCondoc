6fce6e6f6b5abd7ddb26abd3d86cd0a0d860afb4
Gosick96 <arqcompu4@gmail.com>
2019-05-08 17:22:37 -0500

    validacion de formulario

--- src/app/pages/dashboard/users/user-create/user-create.component.ts
+++ src/app/pages/dashboard/users/user-create/user-create.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-user-create',
-  templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.scss']
+    selector: 'app-user-create',
+    templateUrl: './user-create.component.html',
+    styleUrls: ['./user-create.component.scss']
 })
 export class UserCreateComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.form = new FormGroup({
+            nombres: new FormControl('', Validators.required),
+            apellidos: new FormControl('', Validators.required),
+            correo: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    register() {
+        console.log(this.form.value);
+    }
 
 }
--- src/app/pages/dashboard/users/users.module.ts
+++ src/app/pages/dashboard/users/users.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { UsersListComponent } from './users-list/users-list.component';
 import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UserCreateComponent } from './user-create/user-create.component';
 
 @NgModule({
-  declarations: [UsersComponent, UsersListComponent, UserCreateComponent],
+  declarations: [
+    UsersComponent, 
+    UsersListComponent, 
+    UserCreateComponent
+  ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class UsersModule {}
